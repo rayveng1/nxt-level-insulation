fc2b16c1d5b0233a895de0ddb8dbb67bf8b356c6
rayveng1 <rayvengsell@gmail.com>
2024-07-29 00:03:45 -0500

    BIG CHANGES

---
+++ script.js
@@ -0,0 +1,17 @@
+function turnBlue(element){
+    console.log(element.style);
+    element.style.backgroundColor = "var(--primary-color)";
+
+}
+
+function turnBlack(element){
+    console.log(element.style);
+    element.style.backgroundColor = "var(--black)";
+
+}
+
+function turnGrey(element){
+    console.log(element.style);
+    element.style.backgroundColor = "var(--darker-grey)";
+
+}
