00c30046be0d518b1221a06698620c1cdec44e6b
rayveng1 <rayvengsell@gmail.com>
2024-08-01 12:48:54 -0500

    BIG CHANGES

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 function turnBlue(element){
-    console.log(element.style);
     element.style.backgroundColor = "var(--primary-color)";
 
 }
 
 function turnBlack(element){
-    console.log(element.style);
     element.style.backgroundColor = "var(--black)";
 
 }
 
 function turnGrey(element){
-    console.log(element.style);
     element.style.backgroundColor = "var(--darker-grey)";
 
+}
+
+function hoverNavLink(element){
+    element.style.color = "var(--primary-color)";
+    element.style.fontSize = "large";
+}
+
+function unhoverNavLink(element){
+    element.style.color = "white";
+    element.style.fontSize = "medium";
+    
 }
