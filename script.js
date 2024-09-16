4202ffcf8b613f133d4e861e56b9a3f83466ec35
rayveng1 <rayvengsell@gmail.com>
2024-09-15 22:46:49 -0500

    Major changes, added service pages and more!

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function turnBlue(element){
 }
 
 function turnBlack(element){
+    element.style.color = "white"
     element.style.backgroundColor = "var(--black)";
 
 }
@@ -23,3 +24,24 @@ function unhoverNavLink(element){
     element.style.fontSize = "medium";
     
 }
+
+// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+var mainbottom = $('#hero_nav').offset().top + $('#hero_nav').height();
+
+window.onload = function() {
+    // Select the element by its ID (or class, tag, etc.)
+    document.getElementById("nav").style.display = "none";
+};
+
+// on scroll, 
+$(window).on('scroll',function(){
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop < mainbottom) {
+        document.getElementById("nav").style.display = "flex";
+        $('#nav').addClass('nav');
+    } else {
+        $('#nav').removeClass('nav');
+   }
+
+});
